84d9afb4c77b655c648bc3612af617ad555e5e2e
Asım Kaymak <kaymakasm@gmail.com>
2022-07-29 17:20:48 -0700

    actionDate fixed

--- havelsan/src/main/java/org/ros2/rcljava/havelsanros2/actiondate/ActionDate.java
+++ havelsan/src/main/java/org/ros2/rcljava/havelsanros2/actiondate/ActionDate.java
@@ -23,11 +23,21 @@ public class ActionDate {
     public static String longToStringDate(long longDate){
         String converted_date = String.valueOf(longDate);
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
-        LocalDateTime custom_date = LocalDateTime.of(Integer.parseInt(converted_date.substring(8,12)),
-                                                    Integer.parseInt(converted_date.substring(6, 8)),
-                                                    Integer.parseInt(converted_date.substring(4, 6)),
-                                                    Integer.parseInt(converted_date.substring(0, 2)),
-                                                    Integer.parseInt(converted_date.substring(2,4)));
+        LocalDateTime custom_date;
+        if(converted_date.length() == 12){
+            custom_date = LocalDateTime.of(Integer.parseInt(converted_date.substring(8,12)),
+                                            Integer.parseInt(converted_date.substring(6, 8)),
+                                             Integer.parseInt(converted_date.substring(4, 6)),
+                                             Integer.parseInt(converted_date.substring(0, 2)),
+                                             Integer.parseInt(converted_date.substring(2,4)));
+        }
+        else{
+            custom_date = LocalDateTime.of(Integer.parseInt(converted_date.substring(7,11)),
+                                            Integer.parseInt(converted_date.substring(5, 7)),
+                                             Integer.parseInt(converted_date.substring(3, 5)),
+                                             Integer.parseInt(converted_date.substring(0, 1)),
+                                             Integer.parseInt(converted_date.substring(1,3)));
+        }
         return dtf.format(custom_date);
     }
     
